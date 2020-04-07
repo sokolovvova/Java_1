8240e2f1044540f02533b889b8a179665e121d62
Vladimir <vova.sokolov.1997@gmail.com>
2020-04-07 17:05:56 +0300

    Task1: menu v 0.2 + some perfomance improvment when working with big balances

--- src/Week3/Task1/CashMachine.java
+++ src/Week3/Task1/CashMachine.java
@@ -26,24 +26,32 @@ public class CashMachine {
         System.out.println("Баланс купюр в банкомате: ");
         System.out.print("20="+cash20+"шт, ");
         System.out.print("50="+cash50+"шт, ");
-        System.out.print("100="+cash100+"шт.");
-        System.out.println("Всего="+balance+"\n");
+        System.out.print("100="+cash100+"шт, ");
+        System.out.println("Всего="+balance+".\n");
         return true;
     }
 
     public boolean outMoney(int sum){
         Scanner scanner = new Scanner(System.in);
         int[][] cashVar = new int[(cash20+1)*(cash50+1)*(cash100+1)][3];
-        int[][] outVar = new int[cashVar.length][3];
+        int[][] outVar = new int[110][3];
         int i=0,iOut=0,nMin=cash20+cash50+cash100,n,iMin=0;
 
         for(int c20=0;c20<=cash20;c20++){
             for (int c50=0;c50<=cash50;c50++){
                 for(int c100=0;c100<=cash100;c100++){
-                    cashVar[i][0]=c20;
-                    cashVar[i][1]=c50;
-                    cashVar[i][2]=c100;
-                    i++;
+                    if(sum==(c20*20+c50*50+c100*100)){
+                        outVar[iOut][0]=c20;
+                        outVar[iOut][1]=c50;
+                        outVar[iOut][2]=c100;
+                        n=c20+c50+c100;
+                        if(n<nMin){
+                            nMin=n;
+                            iMin=iOut;
+                        }
+                        iOut++;
+                    }
+                    if (iOut>10) break;
                 }
             }
         }
@@ -54,17 +62,7 @@ public class CashMachine {
 
         //возможные варианты выдачи нужной суммы
         for(int[] ar:cashVar){
-            if(sum==(ar[0]*20+ar[1]*50+ar[2]*100)){
-                outVar[iOut][0]=ar[0];
-                outVar[iOut][1]=ar[1];
-                outVar[iOut][2]=ar[2];
-                n=ar[0]+ar[1]+ar[2];
-                if(n<nMin){
-                    nMin=n;
-                    iMin=iOut;
-                }
-                iOut++;
-            }
+
         }
         if(iOut==0){
             System.out.println("Выдать данную сумму имеющимися купюрами невозможно.");
--- src/Week3/Task1/Main.java
+++ src/Week3/Task1/Main.java
@@ -8,14 +8,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while(true){
-            System.out.println("1.Добавить деньги в банкомат.\n2.Наличие купюр в банкомате\n3.Снять деньги\n4.Выход.");
+            System.out.print("1.Добавить деньги в банкомат.\n2.Наличие купюр в банкомате\n3.Снять деньги\n4.Выход.\n> ");
             int n = scanner.nextInt();
             switch(n){
                 case 1:
-                    System.out.println("Введите количество банкнот номиналом : 20 50 100. (Пример \"10 15 10\")");
+                    System.out.print("Введите количество банкнот номиналом : 20 50 100. (Пример \"10 15 10\").\n> ");
                     errCheck(ATM.addMoney(scanner.nextInt(),scanner.nextInt(),scanner.nextInt()));
 
-                    System.out.println("Для продолжение введите 1, для выхода введите 2.");
+                    System.out.print("1.Продолжить.\n2.Выход.\n> ");
                     int i=scanner.nextInt();
                     if(i==1) continue;
                     else if (i==2) break;
@@ -24,7 +24,7 @@ public class Main {
                 case 2:
                     errCheck(ATM.cashBalance());
 
-                    System.out.println("Для продолжение введите 1, для выхода введите 2.");
+                    System.out.print("1.Продолжить.\n2.Выход.\n> ");
                     i=scanner.nextInt();
                     if(i==1) continue;
                     else if (i==2) break;
@@ -34,15 +34,15 @@ public class Main {
                     int s =scanner.nextInt();
                     errCheck(ATM.outMoney(s));
 
-                    System.out.println("Для продолжение введите 1, для выхода введите 2.");
+                    System.out.print("1.Продолжить.\n2.Выход.\n> ");
                     i=scanner.nextInt();
                     if(i==1) continue;
                     else if (i==2) break;
                 case 4:
-                    System.out.println("Завершение работы программы");
+                    System.out.println("Завершение работы программы.");
                     break;
                 default:
-                    System.out.println("Вариант меню не найден, попробуйте снова");
+                    System.out.println("Вариант меню не найден, попробуйте снова.");
             }
             break;
 
